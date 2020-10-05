1701700394c97c3712eba751b427ad3768d4af3f
Mace <maceplus@gmail.com>
2020-10-05 16:54:11 +0100

    test complete

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+  describe "initialized cards" do
+    it "freshly initialized cards should have a balance of 0" do
+      expect(subject.balance).to eq 0
+    end
+  end
+
 
 end
