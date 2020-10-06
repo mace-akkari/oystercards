d23fc35ace3cc9c551f2b510327c760b9a6ac415
Mace <maceplus@gmail.com>
2020-10-06 16:57:30 +0100

    charging for the journey test

--- spec/in_transit_spec.rb
+++ spec/in_transit_spec.rb
@@ -2,12 +2,14 @@ require 'in_transit'
 require 'oystercard'
 
 describe InTransit do
+  let(:intransit) { InTransit.new }
+  let(:oystercard) { Oystercard.new }
   describe 'Sufficient funds' do 
     before do 
       allow_any_instance_of(Oystercard).to receive(:balance).and_return(2)
     end
       it 'is not in transit' do
-        expect(subject.in_journey?).to eq(false)
+        expect(intransit.in_journey?).to eq(false)
       end
 
       it { is_expected.to respond_to :touch_in } 
@@ -16,18 +18,23 @@ describe InTransit do
 
       it 'it activates the card when touched in' do 
         subject.touch_in
-        expect(subject.in_journey?).to eq(true)
+        expect(intransit.in_journey?).to eq(true)
       end
 
       it 'it deactivates the card when touched out' do 
         subject.touch_in
         subject.touch_out
-        expect(subject.in_journey?).to eq(false)
+        expect(intransit.in_journey?).to eq(false)
+      end
+
+      it 'deducts min fare on touch out' do 
+        intransit.touch_in
+        expect{ intransit.touch_out }.to change{ oystercard.balance }.by (InTransit::MINIMUM_FARE)
       end
 end
 
   it 'would raise error if balance is below minimum fare' do 
-    expect { subject.touch_in }.to raise_error "Insufficient funds: Balance less than #{InTransit::MINIMUM_FARE}"
+    expect { intransit.touch_in }.to raise_error "Insufficient funds: Balance less than #{InTransit::MINIMUM_FARE}"
   end
 
 
