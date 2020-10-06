709e309748b311729118ee2f38b6aa5f9ddd7be4
Mace <maceplus@gmail.com>
2020-10-06 14:49:01 +0100

    add MAX_LIMIT

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
   attr_reader :balance
+  MAXIMUM_LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    fail "MAX #{MAXIMUM_LIMIT} EXCEEDED" if exceeded_limit?(money)
     @balance += money
   end 
+
+  private
+  def exceeded_limit?(money)
+    balance + money > MAXIMUM_LIMIT
+  end
 end 
