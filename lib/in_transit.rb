5155bd6a53c575527d9d7b68ebce8f80e783dd22
Mace <maceplus@gmail.com>
2020-10-06 16:40:10 +0100

    add minimum fare fail to method

--- lib/in_transit.rb
+++ lib/in_transit.rb
@@ -1,7 +1,11 @@
+require 'oystercard.rb'
+
 class InTransit
+  MINIMUM_FARE = 1
 
   def initialize
     @activated = false
+    @card = Oystercard.new
   end
 
   def in_journey?
@@ -9,6 +13,8 @@ class InTransit
   end
 
   def touch_in
+    #check_balance - oystercard class
+    fail "Insufficient funds: Balance less than #{InTransit::MINIMUM_FARE}" if @card.balance < MINIMUM_FARE
     @activated = true
   end 
 
